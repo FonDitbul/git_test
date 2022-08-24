253f0e92a8d6c3da7bcc0d2f1298a8d0abffc01a
FonDitbul <niki7084@naver.com>
2022-08-24 22:25:19 +0900

    prettier 적용

--- nest-test/src/app.service.ts
+++ nest-test/src/app.service.ts
@@ -5,19 +5,11 @@ export class AppService {
   getHello(): string {
     console.log('test');
     console.log('test2');
-		console.log('PR 날리고싶어요');
+    console.log('PR 날리고싶어요');
 
-    const abc = 55555
-    console.log(abc)
-    console.log('te' +
-			'sadss' +
-			'ddsadsttes' +
-			'tetst' + 'sdd' +
-			'dte' +
-			'sfsafsafsa' +
-			'tstesfsafaffas' +
-			'afstestes' +
-			't');
+    const abc = 55555;
+    console.log(abc);
+    console.log('te' + 'sadss' + 'ddsadsttes' + 'tetst' + 'sdd' + 'dte' + 'sfsafsafsa' + 'tstesfsafaffas' + 'afstestes' + 't');
     return 'Hello World!';
   }
 }
