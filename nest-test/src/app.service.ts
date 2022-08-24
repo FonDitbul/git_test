8df00dbc041ff3481f016cdf137fc06f69b8c444
FonDitbul <niki7084@naver.com>
2022-08-24 22:07:58 +0900

    github action push in merge 테스트

--- nest-test/src/app.service.ts
+++ nest-test/src/app.service.ts
@@ -5,11 +5,13 @@ export class AppService {
   getHello(): string {
     console.log('test');
     console.log('test2');
-    const abc = 55555;
+    const abc = 55555
     console.log(abc)
     console.log('te' +
 			'sadssddsadsttes' +
-			'tetst' + 'sdddtesfsafsafsatstesfsafaffasafstestes' +
+			'tetst' + 'sdddte' +
+			'sfsafsafsa' +
+			'tstesfsafaffasafstestes' +
 			't');
     return 'Hello World!';
   }
